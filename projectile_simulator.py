484a2f29c3e95e844904c3d58b7e1d18a4b9f8d6
Yar Neprost <yarik@iastate.edu>
2026-05-25 23:27:03 -0500

    Can properly calculate total fly time and hor. displacement

--- projectile_simulator.py
+++ projectile_simulator.py
@@ -2,8 +2,11 @@ import math
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
 
+# Constants:
+GROUND = 0
+
 # initial_height   - Starting height of the bottle's center of mass(CoM) from the ground
-# initial_velocity    - How fast the bottle is moving when it leaves the hand
+# initial_velocity - How fast the bottle is moving when it leaves the hand
 # launch_angle     - The angle at witch the bottle is thrown, relative to the ground
 # angular_velocity - How fast the bottle is spinning when it leaves the hand, in radians per seconds
 # CoM_offset       - Distance from the physical bottom of the bottle to the center of mass
@@ -19,11 +22,29 @@ def main():
     # 1. Get parameters
     parameters = get_input_parameters()
 
-    print("Hello World!")
-
     # 2. Use received parameters.
     print(f"\nSimulation Starting!")
     print(f"Velocity: {parameters.initial_velocity} m/s")
+
+    #3. Find horizonal and verical component of initial_velocity vector
+    angle_rad = math.radians(parameters.launch_angle)
+    component_y = math.sin(angle_rad) * parameters.initial_velocity
+    component_x = math.cos(angle_rad) * parameters.initial_velocity
+
+    # 4. Find total time in the air
+    A = -9.8 / 2
+    B = component_y
+    C = parameters.initial_height - GROUND
+    discriminant = (B**2) - (4 * A * C)
+
+    time_1 = (-B + math.sqrt(discriminant)) / (2 * A)
+    time_2 = (-B - math.sqrt(discriminant)) / (2 * A)
+
+    total_time = max(time_1, time_2)
+    total_displacemnt = total_time * component_x
+
+    print(f"Total time in the air: {total_time:.3f} seconds")
+    print(f"Total horizontal displacement: {total_displacemnt:.3f} meters")
 
 def get_input_parameters() -> Parameters:
     print("--- Enter simulation Parameters ---")
@@ -37,4 +58,3 @@ def get_input_parameters() -> Parameters:
 
 if __name__ == "__main__":
     main()
-
